dbaf161a72e4a31aae276bf135ff72161226a66c
Dhushyanth Ravichandran <dhushyanthravichandran@gmail.com>
2024-08-13 13:02:44 +0530

    feat:bux fix

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,28 @@
-const app=require('./app');
+const app = require('./app');
 const databaseConnect = require('./config/database');
-const path=require('path');
+const path = require('path');
 
 databaseConnect();
 const PORT = process.env.PORT || 3000;
 
-const server=app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-process.on('unhandledRejection',(err)=>{
-  console.log(`Error :${err.message}`);
-  console.log('shutting down due to server unhandledRejection');
-  server.close(()=>{
+// Handling unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log('Shutting down due to unhandled promise rejection');
+  server.close(() => {
     process.exit(1);
-  })
-})
+  });
+});
 
-process.on('uncaughtException',(err)=>{
-  console.log(`Error :${err.message}`);
-  console.log('shutting down due to server unhandledRejection');
-  server.close(()=>{
+// Handling uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log('Shutting down due to uncaught exception');
+  server.close(() => {
     process.exit(1);
-  })
-})
+  });
+});
